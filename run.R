cb23ee68c959359fff5d114ceb219aa642cce354
Chang-Yu Chang <cychang@dept-dynamic-host-222-117.bio.upenn.edu>
2023-10-26 18:14:55 -0400

    update contact info and links

---
+++ run.R
@@ -0,0 +1,7 @@
+#' This script executes both resume.Rmd and cv.Rmd to generate the html document
+#' and generate the pdf from these html
+
+rmarkdown::render(input = "cv.Rmd", output_file = "cv.html")
+rmarkdown::render(input = "resume.Rmd", output_file = "resume.html")
+pagedown::chrome_print(input = "cv.html")
+pagedown::chrome_print(input = "resume.html")
